b9ed1f2663d3cbf21433a4735d91e3278236f1e5
Eric Weise <eric@curemetrix.com>
2019-09-09 18:00:57 -0700

    First version of home page

---
+++ htdocs/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Eric D. Weise - Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/stylesheets/main.css">
+    </head>
+    <?php include('header.php'); ?>
+    <body>
+        <!--TODO: Add a profile picture -->
+        <!--TODO: Add a bio -->
+        <section>
+            <h1>Consulting</h1>
+            <p>I have spent several years as the Lead Software Developer at a small startup company. If you need help with a project please see my <a href="consulting.php">Software Consulting</a> page.</p>
+        </section>
+    </body>
+    <?php include('footer.php'); ?>
+</html>
