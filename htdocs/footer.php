4e04816120b983aaa6575913c6e32ddfef92a246
Eric Weise <eric@curemetrix.com>
2019-09-10 15:42:48 -0700

    simplify footer appearance

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -1,8 +1,6 @@
 <div id="footer">
-    <p>How to find me:</p>
-    <ul>
-        <li>EricDWeise [snail] gmail
-        <li><a class="footer" href="https://github.com/ericdweise">GitHub</a>
-        <li><a class="footer" href="https://www.linkedin.com/in/eric-weise-83a238123/">LinkedIn</a>
-    </ul>
+    <div class="flex-container" id="find-me">
+        <a class="footer" href="https://github.com/ericdweise">GitHub</a>
+        <a class="footer" href="https://www.linkedin.com/in/eric-weise-83a238123/">LinkedIn</a>
+    </div>
 </div>
