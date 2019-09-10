13d62e1726c41cf11758db2cbb6ea769f14fd293
Eric Weise <eric@curemetrix.com>
2019-09-10 11:24:34 -0700

    moved nav-logo out of nav-bar

--- htdocs/header.php
+++ htdocs/header.php
@@ -1,11 +1,10 @@
 <div id="header">
+    <div id="nav-logo">
+        <!-- Placeholder for logo -->
+        LOGO
+    </div>
     <div class="flex-container" id="navbar">
         <!-- TODO: Center this in the device screen -->
-        <div id="nav-logo">
-            <!-- Placeholder for logo -->
-            <h2>LOGO</h2>
-            <!-- TODO: do not include this in the navbar -->
-        </div>
         <a class="header" href="index.php">
             <div class="nav-box">Home</div>
         </a>
