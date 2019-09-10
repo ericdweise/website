e685a9a9b85651a56b52464081253f0809303aee
Eric Weise <eric@curemetrix.com>
2019-09-10 10:20:01 -0700

    Changed header and footer to div, added styling to include navbar

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -1,8 +1,8 @@
-<footer>
+<div id="footer">
     <p>How to find me:</p>
     <ul>
         <li>EricDWeise [snail] gmail
         <li><a class="footer" href="https://github.com/ericdweise">GitHub</a>
         <li><a class="footer" href="https://www.linkedin.com/in/eric-weise-83a238123/">LinkedIn</a>
     </ul>
-</footer>
+</div>
--- htdocs/header.php
+++ htdocs/header.php
@@ -1,5 +1,20 @@
-<header>
-    <!-- Placeholder for the navigation button -->
-    <a class="header" href="index.php">HOME</a>
+<div id="header">
+    <div class="flex-container" id="navbar">
+        <!-- TODO: Center this in the device screen -->
+        <div id="nav-logo">
+            <!-- Placeholder for logo -->
+            <h2>LOGO</h2>
+            <!-- TODO: do not include this in the navbar -->
+        </div>
+        <div class="nav-box">
+            <a class="header" href="index.php">Home</a>
+        </div>
+        <div class="nav-box">
+            <a class="header" href="consulting.php">Consulting</a>
+        </div>
+        <div class="nav-box">
+            <a class="header" href="blog.php">My Thoughts</a>
+        </div>
+    </div>
     <!-- Placeholder for the day/night mode toggle switch -->
-</header>
+</div>
