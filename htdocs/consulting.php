2a05c48df2ceb0c2f3aa4a1c474329e06f90652e
Eric Weise <eric@curemetrix.com>
2019-09-10 11:15:56 -0700

    Incomplete version of consulting.php

---
+++ htdocs/consulting.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title> Eric D. Weise - Consulting</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/stylesheets/main.css">
+    </head>
+    <?php include('header.php'); ?>
+    <body>
+        <h1>Software Development and DevOps Consulting</h1>
+        <p>I have spent the past several years as the Lead Software Developer at a small startup company. I learned how to work in a small company to support research teams by providing operational support and preparing healthcare software for the regulated production environment. If your company needs help in these areas please contact me at EricDWeise[snail]gmail.com.</p>
+        <h2>Specialties</h2>
+        <h3>Test Driven Development</h3>
+        <p></p> <!-- TODO -->
+        <h3>Continuous Integration & Continuous Deployment</h3>
+        <p></p> <!-- TODO -->
+        <h3>Cloud Services</h3>
+        <p>I have used the major cloud service platforms extensively and am very familiar with Amazon Web Services and Digital Ocean.</p>
+        <h3>Regulatory Compliance</h3>
+        <p>Production code hardly ever exists outside a regulated environment. Having written code for a healthcare software package I am familiar working within the bounds of an FDA regulated environment.</p>
+        <h2>Resume</h2>
+        <!-- TODO -->
+        <h2>Portfolio</h2>
+        While much of my work is proprietary or confidential, my personal projects are available on <a href="https://github.com/ericdweise">GitHub</a>.
+    </body>
+    <?php include('footer.php'); ?>
+</html>
