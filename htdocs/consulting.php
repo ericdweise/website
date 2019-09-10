0486797f593b47f9e0eccf6591e19ae5dfc1f8f7
Eric Weise <eric@curemetrix.com>
2019-09-10 12:20:24 -0700

    new class div.page-content to style page

--- htdocs/consulting.php
+++ htdocs/consulting.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" href="assets/stylesheets/main.css">
     </head>
     <?php include('header.php'); ?>
-    <body>
+    <div class="page-content">
         <h1>Software Development and DevOps Consulting</h1>
         <p>I have spent the past several years as the Lead Software Developer at a small startup company. I learned how to work in a small company to support research teams by providing operational support and preparing healthcare software for the regulated production environment. If your company needs help in these areas please contact me at EricDWeise[snail]gmail.com.</p>
         <h2>Specialties</h2>
@@ -23,6 +23,6 @@
         <!-- TODO -->
         <h2>Portfolio</h2>
         While much of my work is proprietary or confidential, my personal projects are available on <a href="https://github.com/ericdweise">GitHub</a>.
-    </body>
+    </div>
     <?php include('footer.php'); ?>
 </html>
--- htdocs/index.php
+++ htdocs/index.php
@@ -7,13 +7,13 @@
         <link rel="stylesheet" href="assets/stylesheets/main.css">
     </head>
     <?php include('header.php'); ?>
-    <body>
+    <div class="page-content">
         <!--TODO: Add a profile picture -->
         <!--TODO: Add a bio -->
         <section>
             <h1>Consulting</h1>
             <p>I have spent several years as the Lead Software Developer at a small startup company. If you need help with a project please see my <a href="consulting.php">Software Consulting</a> page.</p>
         </section>
-    </body>
+    </div>
     <?php include('footer.php'); ?>
 </html>
