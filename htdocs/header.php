14e36d89c1ec7aae0381d9e3de06a72b815ad50a
Eric Weise <eric@curemetrix.com>
2019-09-09 18:00:02 -0700

    First version of header

---
+++ htdocs/header.php
@@ -0,0 +1,5 @@
+<header>
+    <!-- Placeholder for the navigation button -->
+    <a class="header" href="index.php">HOME</a>
+    <!-- Placeholder for the day/night mode toggle switch -->
+</header>
