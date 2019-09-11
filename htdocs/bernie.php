a1586ab882b29766465eb72f6b665bba31989572
Eric Weise <eric@curemetrix.com>
2019-09-11 11:10:43 -0700

    added LabsNMore link on Bernie page

--- htdocs/bernie.php
+++ htdocs/bernie.php
@@ -10,7 +10,7 @@
     <div class="page-content">
         <section>
             <h1>My Compadre</h1>
-            <p>Meet my hiking buddy, Bernie. He entered my life in February 2017. At the time I was fostering dogs through <a href="">Labs and More</a> in San Diego. He was brought up from the streets of Tijuana. He had a form of transmissible cancer calle TVT that maniested as a bloody tumor. After eight months of treatment, which included two rounds of chemotherapy and three courses of radiation therapy he was cured. </p>
+            <p>Meet my hiking buddy, Bernie. He entered my life in February 2017. At the time I was fostering dogs through <a href="https://www.labsandmore.org/">Labs and More</a> in San Diego. He was brought up from the streets of Tijuana. He had a form of transmissible cancer calle TVT that maniested as a bloody tumor. After eight months of treatment, which included two rounds of chemotherapy and three courses of radiation therapy he was cured. </p>
             <p>Despite all his problems, Bernie is the happiest dog I have ever had, and he is a great hiker. He loves dirty socks, belly rubs, eating chicken out of the trash.</p>
             <h2>Is Bernie pollitically active?</h2>
             <p>When I first met Bernie he already shared a name with a certain member of Congress. But the name fits his personality and so it stuck.</p>
