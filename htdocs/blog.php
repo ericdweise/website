903b13c05ffefb0472f1a3e38e0d32a5e77c7d9a
Eric Weise <eric@curemetrix.com>
2019-09-11 00:26:38 -0700

    Added placeholder page for blog

---
+++ htdocs/blog.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Eric D. Weise - Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/stylesheets/main.css">
+    </head>
+    <?php include('header.php'); ?>
+    <div class="page-content">
+        <section>
+            <p>This is a placeholder for my main blog page.</p>
+        </section>
+    </div>
+    <?php include('footer.php'); ?>
+</html>
