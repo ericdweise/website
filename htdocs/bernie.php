cb8d36fe0fa5b0a4ba9d6b079ff348cc6b899258
Eric Weise <eric@curemetrix.com>
2019-09-11 17:39:46 -0700

    Added picture of Bernie

--- htdocs/bernie.php
+++ htdocs/bernie.php
@@ -8,9 +8,10 @@
     </head>
     <?php include('header.php'); ?>
     <div class="page-content">
+        <img class="page-content border circle" src="assets/images/bernie-400.jpg">
         <section>
             <h1>My Compadre</h1>
-            <p>Meet my hiking buddy, Bernie. He entered my life in February 2017. At the time I was fostering dogs through <a href="https://www.labsandmore.org/">Labs and More</a> in San Diego. He was brought up from the streets of Tijuana. He had a form of transmissible cancer calle TVT that maniested as a bloody tumor. After eight months of treatment, which included two rounds of chemotherapy and three courses of radiation therapy he was cured. </p>
+            <p>Meet my hiking buddy, Bernie, who's been following me around since February 2017. At the time I was fostering dogs through <a href="https://www.labsandmore.org/">Labs and More</a> in San Diego. He was brought up from the streets of Tijuana. He had a form of transmissible cancer called TVT. After eight months of treatment, which included two rounds of chemotherapy and three courses of radiation therapy he was cured. </p>
             <p>Despite all his problems, Bernie is the happiest dog I have ever had, and he is a great hiker. He loves dirty socks, belly rubs, eating chicken out of the trash.</p>
             <h2>Is Bernie pollitically active?</h2>
             <p>When I first met Bernie he already shared a name with a certain member of Congress. But the name fits his personality and so it stuck.</p>
