498e22695fed82eea3ba8381a3814548254a49a8
Eric Weise <eric@curemetrix.com>
2019-09-12 11:57:06 -0700

    first blog post

--- htdocs/blog/index.php
+++ htdocs/blog/index.php
@@ -8,10 +8,11 @@
     </head>
     <?php include('../header.php'); ?>
     <div class="page-content">
-        <p>This is a placeholder for my main blog page.</p>
+        <a href="posts/2019-09-11.php"><h3>Why am I building this?</h3>
         <!-- TODO: list blog posts in the posts/ directory, latest post first -->
         <!-- TODO: Display a short snippet from each post -->
         <!-- TODO: Add a search function, based on key-word -->
+        <!-- TODO: Add RSS feed -->
     </div>
     <?php include('../footer.php'); ?>
 </html>
---
+++ htdocs/blog/posts/2019-09-11.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Eric D. Weise - Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/assets/stylesheets/main.css">
+    </head>
+    <?php include('../../header.php'); ?>
+    <div class="page-content">
+        <!-- TODO: Make the post title a required variable -->
+        <h1>Why am I building this?</h1>
+        <!-- TODO: make blog date a required variable -->
+        <h2 class="blog-date">2019-09-12</h2>
+        <p>
+        Seriously, why? It is late and I don't need a personal website.
+        I have been working on this site for the past week and it has been slow, tedious, and at points aggravating work.
+        I didn't know anything about writing or hosting a website seven days ago, and learning the nuances of Apache, PHP, and CSS aren't immediately useful to me.
+        So, the question remains: Why am I up at 01:30 doing this?
+        <p>
+        First, let me say that I wouldn't be working on this now if I didn't have extra time on my hands.
+        A week and a half ago the company I was working for ran out of money and I am currently on an unexpected vacation.
+        This project wouldn't have come about if I were still putting as many hours into work as I was two weeks ago.
+        Partially, working on this website is a way to keep myself occupied.
+        But why work on a website?
+        There are many other things I can work on that are more beneficial to the world.
+        A blog seems like a vain &#8212; and maybe useless &#8212; drain of my energy.
+        <p>
+        In the past I have thought about writing my own blog.
+        Having a platform to put my thoughts out in the world was appealing.
+        Not that I think that anyone will care about what I have to say;
+        The internet won't find my banal musings more interesting than the carefully curated videos of <a href="https://www.youtube.com/watch?v=pXv44YL_Gio">cats reacting to cucumbers</a>, and I don't have any particularly poignant or novel ideas worth sharing.
+        Then, why do I want to put myself on the <i>world wide web</i> where trolls, future employers, and my frenemies can easily find my mistakes?
+        It is because even a little bit of visibility will help me refine my thoughts and be more intentional with the content I make.
+        <p>
+        Like I said above, I don't expect the traffic to my website to be large.
+        In fact, I only expect people who know me to come here. (Hi, Mom and Dad!)
+        This is going to be my online journal.
+        I believe that The act of writing down thoughts helps you work through problems.
+        Often times revisiting problems can help you think about them in new ways or with a fresh perspective.
+        My journaling, however, is not very consistent, and when I do write my thoughts come out so jumbled and non-linear mess that it is difficult to draw a conclusion.
+        It is the kind of aimless banter that my grandpa would call "diarrhea of the mouth."
+        Putting my thoughts onto a website where the wider public can view it &#8212; trolls and all &#8212; will make me think harder about what I am writing.
+        This in turn will help me figure out why I do what I do.
+        <p>
+        Or, maybe I just need a different hobby.
+        I will let you all know soon!
+        </p>
+    </div>
+    <?php include('../../footer.php'); ?>
+</html>
