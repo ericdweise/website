60fbb13852204f86ca30fdcbe8b197dfc6809c2e
Eric Weise <eric@curemetrix.com>
2019-09-11 17:41:10 -0700

    longer bio on Home page

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,14 +11,10 @@
         <img class="page-content border circle" src="assets/images/headshot-400px.png">
         <!--TODO: Add a profile picture -->
         <!--TODO: Add a bio -->
-        <section>
             <h1>About Me</h1>
-                <p>I have a dog named <a href="bernie.php">Bernie</a>.</p>
-        </section>
-        <section>
+                <p>Hi, my name is Eric, and this is my personal website. If you're here to learn about me you can here are the basics. I enjoy being outdoors biking, backpacking, and surfing. I have a mild obsession with the TV show Firefly, and I have a dog named <a href="bernie.php">Bernie</a>. Wherever I live I need to have a garden, whether it's a yard or just a few small planters. </p>
             <h1>Consulting</h1>
             <p>I have spent several years as the Lead Software Developer at a small startup company. If you need help with a project please see my <a href="consulting.php">Software Consulting</a> page.</p>
-        </section>
     </div>
     <?php include('footer.php'); ?>
 </html>
