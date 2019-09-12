0eb5f1ee02d9566661202950b5efc3e8c2f76684
Eric Weise <eric@curemetrix.com>
2019-09-12 00:13:44 -0700

    fixed typo

--- htdocs/bernie.php
+++ htdocs/bernie.php
@@ -14,7 +14,7 @@
             <p>Meet my hiking buddy, Bernie, who's been following me around since February 2017. At the time I was fostering dogs through <a href="https://www.labsandmore.org/">Labs and More</a> in San Diego. Bernie was a stray dog from Tijuana, and he came to us with a form of transmissible cancer called TVT. He stayed with me while he was being treated, a process that took eight months and which included two rounds of chemotherapy and three courses of radiation therapy. After he was cured I decided that he was going to stay with me indefinitely.</p>
             <p>No matter what life throws at Bernie he is the happiest dog. He loves dirty socks, belly rubs, and occasionally eating out of the trash.</p>
             <h2>Is Bernie politically active?</h2>
-            <p>People ask me a lot if Bernie was named after a certain member of Congress. I usually explain that he does have some common ground with the famouns Senator: They have the same hairstyle! However, Bernie, like me, finds the incessant bickering in politics to be overwhelming, and so he has no aspirations for higher office.</p>
+            <p>People ask me a lot if Bernie was named after a certain member of Congress. I usually explain that he does have some common ground with the famous Senator: They have the same hairstyle! However, Bernie, like me, finds the incessant bickering in politics to be overwhelming, and so he has no aspirations for higher office.</p>
         </section>
     </div>
     <?php include('footer.php'); ?>
