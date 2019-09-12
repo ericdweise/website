ccf04417ffd9287678a85c44a152313bfc74663c
Eric Weise <eric@curemetrix.com>
2019-09-12 00:06:55 -0700

    links -> absolute in header

---
+++ htdocs/blog/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Eric D. Weise - Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/assets/stylesheets/main.css">
+    </head>
+    <?php include('../header.php'); ?>
+    <div class="page-content">
+        <p>This is a placeholder for my main blog page.</p>
+        <!-- TODO: list blog posts in the posts/ directory, latest post first -->
+        <!-- TODO: Display a short snippet from each post -->
+        <!-- TODO: Add a search function, based on key-word -->
+    </div>
+    <?php include('../footer.php'); ?>
+</html>
--- htdocs/header.php
+++ htdocs/header.php
@@ -1,12 +1,12 @@
 <div id="header">
     <div class="flex-container" id="navbar">
-        <a class="header" href="index.php">
+        <a class="header" href="/">
             <div class="nav-box">Home</div>
         </a>
-        <a class="header" href="consulting.php">
+        <a class="header" href="/consulting.php">
             <div class="nav-box">Consulting</div>
         </a>
-        <a class="header" href="blog.php">
+        <a class="header" href="/blog/">
             <div class="nav-box">My Thoughts</div>
         </a>
     </div>
