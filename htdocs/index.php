5894fef522ecb0e657cd6ba4c169962fa5cf2d45
Eric Weise <eric@curemetrix.com>
2019-09-15 21:44:38 -0700

    Added profile picture and bio to hopepage

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,12 +9,33 @@
     <?php include('header.php'); ?>
     <div class="page-content">
         <img class="page-content border circle" src="assets/images/headshot-400px.png">
-        <!--TODO: Add a profile picture -->
-        <!--TODO: Add a bio -->
-            <h1>About Me</h1>
-                <p>Hi, my name is Eric, and this is my personal website. If you're here to learn about me you can here are the basics. I enjoy being outdoors biking, backpacking, and surfing. I have a mild obsession with the TV show Firefly, and I have a dog named <a href="bernie.php">Bernie</a>. Wherever I live I need to have a garden, whether it's a yard or just a few small planters. </p>
-            <h1>Consulting</h1>
-            <p>I have spent several years as the Lead Software Developer at a small startup company. If you need help with a project please see my <a href="consulting.php">Software Consulting</a> page.</p>
+        <p>
+        This site is a partial collection of my thoughts and work, and is an attempt to be the center of my digital presence.
+        </p>
+        <p>
+        If you're here to learn about me here are the basics.
+        I enjoy being outdoors and my favorite things to do are biking, backpacking, and surfing.
+        I have a mild obsession with the TV show Firefly, and I have a dog named <a href="bernie.php">Bernie</a>.
+        Wherever I live I need to have a garden, whether it's a full yard or just a few small planters on a patio.
+        </p>
+        <p>
+        I am always <i>doing things the hard way</i>.
+        I learn best by jumping in to a project head first and making mistakes along the way.
+        To me, the process is more important than the product, and how something is done is as important as finishing it.
+        </p>
+        <p>
+        I believe that the energy you bring to a room, relationship, or company will circle back to you.
+        Everyone around you breathes the same air, and happiness, confidence, grit, and despair are all contagious.
+        </p>
+        <p>
+        My closest friends are people who find their own, unique way of doing things.
+        They don't usually find the easiest, most straight forward way, but they have interesting stories and are the best teachers.
+        In turn, I hope to teach the people around me all the interesting things I have learned.
+        </p>
+        <h1>Consulting</h1>
+        <p>
+        I have spent several years as the Lead Software Developer at a small startup company.
+        If you need help with a project please see my <a href="consulting.php">Software Consulting</a> page.
     </div>
     <?php include('footer.php'); ?>
 </html>
