a1a2c1f3a7198ef5d2d7495a66aea09a1b186ae8
Eric Weise <eric@curemetrix.com>
2019-09-15 21:41:24 -0700

    reformatted consulting.php sentences

--- htdocs/consulting.php
+++ htdocs/consulting.php
@@ -9,34 +9,51 @@
     <?php include('header.php'); ?>
     <div class="page-content">
         <h1>Software Development and DevOps Consulting</h1>
-        <p>I started my career as a Software Developer at a small startup company right after I graduated from university in 2015. Originally I was hired as an algorithm developer, but as our product and my codebase grew I began working more and more packaging and deploying our product to customers. For the last several years my responsibilities have been to to ensure the integrity of our products and the continuity of our services to customers.</p>
-        <p>If your company needs help in these areas please contact me at EricDWeise @ gmail.com.</p>
+        <p>
+        I started my career as a Software Developer at a small startup company right after I graduated from university in 2015.
+        Originally I was hired as an algorithm developer, but as our product and my codebase grew I began working more and more packaging and deploying our product to customers.
+        For the last several years my responsibilities have been to ensure the integrity of our products and the continuity of our services to customers.
+        </p>
+        <p>
+        If your project or company needs help in these areas please contact me at EricDWeise @ gmail.com.
+        </p>
         <h2>Specialties</h2>
         <h3>Regulatory Compliance</h3>
-        <p>Production code hardly ever exists outside a regulated environment. Having written code for a healthcare software package I am familiar working within the bounds of FDA regulation. This includes</p>
+        <p>
+        Production code hardly ever exists outside a regulated environment.
+        I was the lead developer a healthcare software packagem so I am familiar working within the bounds of FDA regulation.
+        If your product needs to conform to a strict regulatory criteria I can help, particulary in the following areas:
+        </p>
         <ul>
             <li>Developing production level code
-            <li>Writing unit tests in accordance with Failure Mode and Effects Analysis (FMEA)
-            <li>Corrective and Preventative Action (CAPA)
+            <li>Writing unit tests and building your Failure Mode and Effects Analysis (FMEA)
+            <li>Applying Corrective and Preventative Action (CAPA)
             <li>Preparing documentation for FDA applications
         </ul>
         <h3>Cloud Services</h3>
-        <p>I have deployed products to Amazon Web Services and have used it extensively in my profesional and personal work. Besides AWS I have also used GoogleCloud and Digital Ocean. Through these experiences I have learned and can help you with:</p>
+        <p>
+        I have deployed products to Amazon Web Services and have used it extensively in my profesional and personal work.
+        Besides AWS I have also used GoogleCloud and Digital Ocean.
+        Through these experiences I have learned and can help you with:
+        </p>
         <ul>
             <li>Serverless Architecture (AWS Lambda)
             <li>Leveraging and swarming containers (Docker)
             <li>Continuous Integration and Continuous Deployment (CI/CD)
         </ul>
         <h3>Development Methods</h3>
-        <p>My previous projects have been with small, integrated teams. This has required me and my co-workers to wear many hats and work in close support of one another. The easiest way I have found to do this is by setting up a rigorous development environment. I use the following organizational tools:</p>
+        <p>
+        My previous projects have been with small, integrated teams.
+        This has required my co-workers and me to wear many hats and work in close support of one another.
+        The easiest way I have found to do this is by setting up a rigorous development environment.
+        I use the following organizational tools:
+        </p>
         <ul>
-            <li>Agile development (in the form of Agility)
+            <li>Agile development (in the form of <a href="https://pragdave.me/blog/2014/03/04/time-to-kill-agile.html">Agility</a>)
             <li>Source Control using Git and Git Flow
             <li>Test driven development
             <li>Peer reviewed code
         </ul>
-        <!-- <h2>Resume</h2> -->
-        <!-- TODO -->
         <h2>Portfolio</h2>
         While much of my professional work is proprietary and confidential, my personal projects are available on <a href="https://github.com/ericdweise">GitHub</a>.
     </div>
