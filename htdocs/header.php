eb252d90c52788a1525054b2c04be8469e2fb5ea
Eric Weise <eric@curemetrix.com>
2019-10-05 00:23:44 -0700

    background color to white, added title to header

--- htdocs/header.php
+++ htdocs/header.php
@@ -1,9 +1,9 @@
 <div id="header">
+    <div id="title">
+        <a class="title" href="/">Eric Weise</a>
+    </div>
     <div class="flex-container" id="navbar">
-        <a class="header" href="/">
-            <div class="nav-box">Home</div>
-        </a>
-        <a class="header" href="/consulting.php">
+        <a class="header" href="/consulting/">
             <div class="nav-box">Consulting</div>
         </a>
         <a class="header" href="/blog/">
