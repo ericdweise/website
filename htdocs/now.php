59c6151b3b8fed896096b9f4eeed99d35cde9403
Eric Weise <ericdweise@gmail.com>
2020-02-06 16:44:47 -0800

    Now page - work in progres 1

---
+++ htdocs/now.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Eric D. Weise - Now</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/stylesheets/main.css">
+    </head>
+    <?php include('header.php'); ?>
+    <div class="page-content">
+        This page was inspired when I heard Derek Sivers interviewed by Tim Ferriss.
+https://tim.blog/2016/11/21/tools-of-titans-derek-sivers-distilled/
+I thought this is a great way to keep myself on track and help people people.
+So, I am joining the nownownow.com movement!
+        <h2>What I'm working on</h2>
+        <ul>
+            <li> Training for a triathalon. I haven't signed up for one yet, but I have told myself I will complete one by the end of the 2020! I will put up a page to track my progress soon.
+            <li> Working on my master's in Electrical Engineering at UCSD.
+            <li> Working with the Jacobs Graduate Student Council as the <em>Industry and Entrepreneurial Chair</em>. Currently my goals are to increase student engagement with industry and securing funding to continue the work of the JGSC next year.
+        </ul>
+        <h2>What I'm reading</h2>
+        <ul>
+            <li> <em>The courage to be disliked</em> by Ichiro Kishimi and Fumitake Toga
+        </ul>
+    </div>
+    <?php include('footer.php'); ?>
+</html>
