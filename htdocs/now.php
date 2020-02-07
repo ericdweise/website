cb65c03cd79582872609a985c11b0a96f63ed33e
Eric Weise <ericdweise@gmail.com>
2020-02-06 23:21:19 -0800

    Now: February 6, 2020

--- htdocs/now.php
+++ htdocs/now.php
@@ -8,20 +8,30 @@
     </head>
     <?php include('header.php'); ?>
     <div class="page-content">
-        This page was inspired when I heard Derek Sivers interviewed by Tim Ferriss.
-https://tim.blog/2016/11/21/tools-of-titans-derek-sivers-distilled/
-I thought this is a great way to keep myself on track and help people people.
-So, I am joining the nownownow.com movement!
+        This page was inspired when I listened to Derek Sivers's interview with Tim Ferriss 
+        <a href="https://tim.blog/2015/12/14/derek-sivers-on-developing-confidence-finding-happiness-and-saying-no-to-millions/">(interview number 125)</a>.
+        I thought this is not only a great tell people what I'm working on but also to remind myself what's important.
+        So, I am joining the <a href="https://nownownow.com/about">NowNowNow</a> movement.
         <h2>What I'm working on</h2>
         <ul>
+            <li> Persuing a master's in Electrical Engineering with a specialization in Applied Physics at UC San Diego.
             <li> Training for a triathalon. I haven't signed up for one yet, but I have told myself I will complete one by the end of the 2020! I will put up a page to track my progress soon.
-            <li> Working on my master's in Electrical Engineering at UCSD.
-            <li> Working with the Jacobs Graduate Student Council as the <em>Industry and Entrepreneurial Chair</em>. Currently my goals are to increase student engagement with industry and securing funding to continue the work of the JGSC next year.
+            <li> Working with the Jacobs Graduate Student Council as the <em>Industry and Entrepreneurial Chair</em>.
+                Currently my goals are to increase student engagement with industry, particularly with startups, and securing funding for JGSC to continue operating next year.
+            <li> Starting a software development consulting business.
+                See my <a href="/consulting/">Consulting</a> page for more information!
+            <li> Being more grounded in nature.
+                I haven't been backpacking in over a year and I feel like I'm starting to forget what it is like to be wholly imersed in wilderness.
+                I'm tentatively planning 3 backpacking trips in the next few months.
+                Check out my <a href="/adventures/">adventures</a> as I write about them!
         </ul>
         <h2>What I'm reading</h2>
         <ul>
-            <li> <em>The courage to be disliked</em> by Ichiro Kishimi and Fumitake Toga
+            <li> <em>The Courage to Be Disliked</em> by Ichiro Kishimi and Fumitake Toga
+            <li> <em>Brave New World</em> by Aldous Huxley
+            <li> <em>Perfectly Reasonable Deviations (From the Beaten Path)</em> by Richard Feynman
         </ul>
+        Last updated: February 6, 2020
     </div>
     <?php include('footer.php'); ?>
 </html>
