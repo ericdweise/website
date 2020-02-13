68d087fd36d56d318b3d6de2452069de6acabd1c
Eric Weise <ericdweise@gmail.com>
2020-02-13 00:25:16 -0800

    Page to view logo

---
+++ htdocs/logo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Eric D. Weise - Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/stylesheets/main.css">
+    </head>
+    <?php include('header.php'); ?>
+    <div class="page-content">
+        <?php echo file_get_contents("assets/icons/logo-footer.svg"); ?>
+    </div>
+    <?php include('footer.php'); ?>
+</html>
