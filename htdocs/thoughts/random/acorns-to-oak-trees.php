601421104d5534dc95346698bd2d57e0475727a7
Eric Weise <ericdweise@gmail.com>
2020-02-22 21:21:45 -0800

    Thoughts: Acorns to Oak Trees

---
+++ htdocs/thoughts/random/acorns-to-oak-trees.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Eric D. Weise - Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/assets/stylesheets/main.css">
+    </head>
+    <?php include('../../header.php'); ?>
+    <div class="page-content">
+        <!-- TODO: Make the post title a required variable -->
+        <h1>Acorns to Oak Trees</h1>
+        <!-- TODO: make blog date a required variable -->
+        <img class="page-content border circle" src="/assets/images/oak-trees-1.jpg">
+        <h2 class="blog-date">2020-02-22</h2>
+        <p>
+        People often compare to boring things to "watching the grass grow."
+        I think that people who say this don't spend enough time gardening.
+        Last fall I grabbed a handful of acorns while hiking in the San Diego mountains and potted them.
+        Just last week they started sprouting and it is really exciting to see these little guys get going!
+        When they get big enough I'm going to plant them in the open space near my condo.
+        </p>
+    </div>
+    <?php include('../../footer.php'); ?>
+</html>
