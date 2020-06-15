50059fe18ad0ca2195e0d92268083db9369e6e61
Eric Weise <ericdweise@gmail.com>
2020-06-15 14:48:40 -0700

    Updated now page

--- htdocs/now/index.php
+++ htdocs/now/index.php
@@ -17,9 +17,7 @@
         <h2>What I'm working on</h2>
         <ul>
             <li> Pursuing a master's in Electrical Engineering with a specialization in Applied Physics at UC San Diego.
-            <li> Training for a triathlon. I haven't signed up for one yet, but I have told myself I will complete one by the end of the 2020! I will put up a page to track my progress soon.
-            <li> Working with the Jacobs Graduate Student Council as the <em>Industry and Entrepreneurial Chair</em>.
-                Currently my goals are to increase student engagement with industry, particularly with startups, and securing funding for JGSC to continue operating next year.
+            <li> Training for a triathlon. I hoped to do a sign up for my first tri at the end of June, but the current crisis changed that plan. So, I'm going to do an un-official tri sometime over the summer. Hopefully the pools open up sometime otherwise I'm going to have to learn how to swim in the ocean without a surfboard.
             <li> Starting a software development consulting business.
                 See my <a href="/consulting/">Consulting</a> page for more information!
             <li> Being more grounded in nature.
@@ -29,21 +27,23 @@
         </ul>
         <h2>What I'm reading</h2>
         <ul>
-            <li> <em>The Courage to Be Disliked</em> by Ichiro Kishimi and Fumitake Toga
+            <li> <em>All These Shiny Worlds</em>, the 2016 ImerseOrDie anthology
             <li> <em>Perfectly Reasonable Deviations (From the Beaten Track)</em> by Richard Feynman
+            <li> <em>The Hard Thing About Hard Things</em> by Andrew Horowitz
+            <li> <em>The Four Hour Work Week</em> by Tim Ferriss
         </ul>
         <h2>What I'm Listening To</h2>
         <p>
         Normally when I'm in a <em>get stuff done</em> mood I turn to fast, distorted music like
         <a href="https://youtu.be/sAsMUXuBeB8?t=20">Angel Vivaldi</a>.
-        I feel like fast cadence chanels into fast work.
+        I feel like fast cadence channels into fast work.
         Recently, however, I've been listening to slower music.
         I find that it gives me a lot of focused and sustained energy.
         Two playlists that I've been playing pretty much nonstop are
         <a href="https://www.youtube.com/channel/UCY3cAFsquIk7VGMuk-V8S3g/featured">Meute</a> and 
         <a href="https://www.youtube.com/user/rpoland">Candyrat Records</a>.
         </p>
-        Last updated: March 4, 2020
+        Last updated: June 15, 2020
     </div>
     <?php include('../footer.php'); ?>
 </html>
