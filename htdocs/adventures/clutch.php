4d6d4bdd96a03141d1404252f1adb892ae4f4d14
Eric Weise <ericdweise@gmail.com>
2020-07-14 00:55:46 -0700

    starting writing clutch page

---
+++ htdocs/adventures/clutch.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Eric D. Weise - Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/assets/stylesheets/main.css">
+    </head>
+    <?php include('../header.php'); ?>
+    <div class="page-content">
+<h1>A Housebound Adventure</h1>
+<p>
+I have a 2008 Subaru Impreza.
+This car and I have been through thick and thin together.
+We have a fond history, but, the years are starting to take their toll.
+I always take care of her -- regular fluid changes, maintenance, and washes -- but entropy is relentless.
+Last March, just as shelter in place was being rolled out, my clutch stopped working.
+I was at the tail end of a 600 mile road trip up the coast and in literally the last mile I discovered I couldn't shift gears.
+While I was happy I didn't get stuck in the middle of Los Angeles, serious car trouble was not something I wanted to deal with.
+</p>
+<p>
+I knew enough about cars to make them go forwards and keep them from moving too far sideways, but I was out of my league with this problem.
+However, being a resourceful millenial, I turned to Google.
+After an hour searching for terms like "I can't shift when engine is on" I had narrowed my problem down to four possible sources:
+<ul>
+  <li> a depleted clutch fluid,
+  <li> a busted master and/or slave cylinders,
+  <li> a broken clutch diaphram, or
+  <li> a trashed throwout bearing.
+</ul>
+(In the end it turned out it wasn't any of those things.)
+</p>
+<p>
+My parents raised me to be fiercly independent, (sometimes to a fault.)
+
+</p>
+<p>
+</p>
+<p>
+</p>
+    </div>
+    <?php include('../footer.php'); ?>
+</html>
