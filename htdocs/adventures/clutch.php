637244139426fad573c2497d5454448e31536863
Eric Weise <ericdweise@gmail.com>
2020-07-31 00:04:52 -0700

    clutch text
    always label everything
    
    transmission drop

--- htdocs/adventures/clutch.php
+++ htdocs/adventures/clutch.php
@@ -5,39 +5,119 @@
         <title>Eric D. Weise - Blog</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/assets/stylesheets/main.css">
+        <link rel="stylesheet" href="/assets/stylesheets/flickity.css">
     </head>
     <?php include('../header.php'); ?>
     <div class="page-content">
-<h1>A Housebound Adventure</h1>
-<p>
-I have a 2008 Subaru Impreza.
-This car and I have been through thick and thin together.
-We have a fond history, but, the years are starting to take their toll.
-I always take care of her -- regular fluid changes, maintenance, and washes -- but entropy is relentless.
-Last March, just as shelter in place was being rolled out, my clutch stopped working.
-I was at the tail end of a 600 mile road trip up the coast and in literally the last mile I discovered I couldn't shift gears.
-While I was happy I didn't get stuck in the middle of Los Angeles, serious car trouble was not something I wanted to deal with.
-</p>
-<p>
-I knew enough about cars to make them go forwards and keep them from moving too far sideways, but I was out of my league with this problem.
-However, being a resourceful millenial, I turned to Google.
-After an hour searching for terms like "I can't shift when engine is on" I had narrowed my problem down to four possible sources:
-<ul>
-  <li> a depleted clutch fluid,
-  <li> a busted master and/or slave cylinders,
-  <li> a broken clutch diaphram, or
-  <li> a trashed throwout bearing.
-</ul>
-(In the end it turned out it wasn't any of those things.)
-</p>
-<p>
-My parents raised me to be fiercly independent, (sometimes to a fault.)
-
-</p>
-<p>
-</p>
-<p>
-</p>
+      <h1>Mechanical Misadventures</h1>
+      <p>How the internet taught me something I am entirely unqualified to do.</p>
+      <div class="container-slides" data-flickity='{ "cellAlign": "center", "wrapAround": true, "lazyLoad": 2, "pageDots": false }'>
+          <div class="carousel-cell"><img class="carousel-img" src="/assets/images/2020-clutch/clutch-01.jpg" ></div>
+          <div class="carousel-cell"><img class="carousel-img" src="/assets/images/2020-clutch/clutch-02.jpg" ></div>
+          <div class="carousel-cell"><img class="carousel-img" src="/assets/images/2020-clutch/clutch-03.jpg" ></div>
+          <div class="carousel-cell"><img class="carousel-img" src="/assets/images/2020-clutch/clutch-04.jpg" ></div>
+          <div class="carousel-cell"><img class="carousel-img" src="/assets/images/2020-clutch/clutch-05.jpg" ></div>
+          <div class="carousel-cell"><img class="carousel-img" src="/assets/images/2020-clutch/clutch-06.jpg" ></div>
+      </div>
+      <p>
+      I'm not a "car guy," but I love my car.
+      I have a stock Subaru Impreza hatch back.
+      This car got me through high school, college, and my first job.
+      That's twelve years, and counting.
+      And, I'm not done counting yet.
+      </p>
+      <p>
+      In March, 2020, just as the shelter in place orders were going into effect, my shifter stopped working.
+      It happened suddenly;
+      I was travelling back to San Diego after visiting my parents.
+      It's a 300 mile drive from SLO to SD, and at the second to last stoplight I couldn't get into gear.
+      Eventually I got into second and managed to grind into my apartment without stopping.
+      But now what?
+      </p>
+      <p>
+      I am mechanically inclined enough to do basic car maintenance.
+      I grew up changing my own fluids, brake pads, and once a brake rotor, but I was out of my depth when it came to this.
+      I have never worked on a transmission, and I had no clue how to even diagnose what was going on.
+      So, like any good Millenial, I turned to the internet.
+      Searching forums and Googling my symptoms I came up with a list of potential problems.
+      After I figured out that the clutch hydrolics weren't the culprit the only other solutions involved dropping the transmission.
+      At this point I was left with two options:
+      <ol>
+      <li> tow my car to a shop and have a professional do the work, or
+      <li> roll up my sleeves and do it myself.
+      </ol>
+      I am an infuriating mixture of stubborn and cheap, so I went with the second option.
+      </p>
+      <p>
+      One of the best parts about living in the information age is that you can find anything on the internet.
+      YouTube saved my bacon several times during this project.
+      It is amazing how many people put out great tutorials online.
+      Especially since Subaru charges $35 to rent their factory service manual for 72 hours!
+      I have a lot of respect for the professionals who put their knowledge online for kludges like me to use.
+      You can find a list of my resources at the bottom of the page.
+      </p>
+      <p>
+      I don't want this page to be a boring list of technical details, so I'll finish up with a few of the lessons I learned.
+      </p>
+      
+      <h4>Lesson 1 - Car work isn't that hard</h4>
+      <p>
+      Auto maintenance has always been an enigma to me.
+      I was never taught how to work on my car, and for the most part they just work.
+      Taking on this serious of project was incredibly daunting at first, but as I got my hands dirty things started to make sense.
+      The biggest takeaway is this:
+      As long as you have time, patience, and the internet anything is possible.
+      </p>
+      
+      <h4>Lesson 2 - Buy the tools you need</h4>
+      <p>
+      Like I said before, I am cheap and stubborn; here is the perfect illustration:
+      I don't have torque wrenches, instead I use a steel pipe and a luggage scale.
+      I can't tell you how many times I dropped my socket wrench before my neighbor lent me his torque wrenches.
+      Thanks to Justin I saved a lot of time and energy (and I now own my own torque wrench.)
+      </p>
+      <p>
+      Also, the only jack I could get shipped to my apartment was a motorcycle lift.
+      This did the trick, but a transmission jack would have made the job much easier.
+      With my setup I had to roll the transmission into place with wooden dowels and guide it with rachet straps.
+      It would have been a lot less sketchy if I had the right jack.
+      </p>
+      
+      <h4>Lesson 3 - When a professional says you can finish a job in 8 hours...</h4>
+      <p>
+      ...it will probably take you 3 times that.
+      It took me 27.5 hours from first bolt off to test drive.
+      This was spread out over 8 days and does include set up and break down time, but I still spent three times longer than BriansMobile1 said I should.
+      And this doesn't count time researching or going to the store.
+      </p>
+      
+      <h4>Lesson 4 - Always label everything</h4>
+      <p>
+      The last thing you want at the end of a project like this is to have three extra screws.
+      </p>
+      
+      <h4>Lesson 5 - 6 point wrenches are far superior to 12 point wrenches</h4>
+      <p>
+      Do not use 12 point wrenches on old bolts.
+      You will wind up busting them.
+      </p>
+      
+      <p>
+      The total final cost of the parts and extra tools I bought came out to $647, less than half the cheapest quote I found. 
+      This isn't a project I want to take on again soon, but I'm glad I did it.
+      Thankfully my car has been running for a month with no more problems.
+      </p>
+      
+      <h2>References</h2>
+      <ul>
+        <li> <a href="https://www.youtube.com/watch?v=hwy9leulOYU">BriansMobile1 clutch replacement part 1</a>
+        <li> <a href="https://www.youtube.com/watch?v=C_CSbYJU2RQ">BriansMobile1 clutch replacement part 2</a>
+        <li> <a href="https://www.youtube.com/watch?v=qTrK_3Hf6b4">Rust Belt Auto</a>
+        <li> <a href="https://www.youtube.com/watch?v=5GgfLJufizU">Gold Guy</a>
+        <li> <a href="https://www.youtube.com/watch?v=xnYe4GZMvSo">Exedy Install Instructions</a>
+        <li> <a href="https://www.youtube.com/watch?v=qBajwSZUNio">A way to remove the CV axles that I should not have needed to look up</a>
+      </ul>
     </div>
     <?php include('../footer.php'); ?>
+    <script src="/assets/javascript/flickity.pkgd.js"></script>
 </html>
